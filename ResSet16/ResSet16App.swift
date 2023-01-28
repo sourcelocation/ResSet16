51df95574863cf17fb3c8446e02c8ba3338649b0
Matthew Anisovich <anisovich.matthew@gmail.com>
2023-01-28 15:18:10 +0000

    Initial Commit

---
+++ ResSet16/ResSet16App.swift
@@ -0,0 +1,17 @@
+//
+//  ResSet16App.swift
+//  ResSet16
+//
+//  Created by sourcelocation on 28/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ResSet16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
