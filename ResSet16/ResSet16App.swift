15ff117099ef0175d8b164791d39540fa872abf7
Matthew Anisovich <anisovich.matthew@gmail.com>
2023-01-31 16:10:13 +0000

    Allow using 16.2 dev beta 1

--- ResSet16/ResSet16App.swift
+++ ResSet16/ResSet16App.swift
@@ -27,7 +27,7 @@ struct ResSet16App: App {
 #if targetEnvironment(simulator)
 #else
         if #available(iOS 16.2, *) {
-            UIApplication.shared.alert(title: "Not Supported", body: "This version of iOS is not supported.", withButton: false)
+            UIApplication.shared.alert(title: "Not Supported", body: "This version of iOS is not supported.")
         } else {
             do {
                 // TrollStore method
